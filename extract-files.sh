10d8ae4a58903630d451999642b64a927844b0cb
Arian <arian.kulmer@web.de>
2024-09-06 14:01:17 +0200

    garnet: Use libhidlbase_shim for vendor.libdpmframework
    Change-Id: Iefe50b82b5b13bbdee10819a0c60c14240fab7ce
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup() {
         vendor/etc/init/mi_ric.rc)
             sed -i s/seclabel\ u:r:mi_ric:s0//g "${2}"
             ;;
+        vendor/lib64/vendor.libdpmframework.so)
+            "${PATCHELF_0_17_2}" --add-needed "libhidlbase_shim.so" "${2}"
+            ;;
     esac
 }
 
