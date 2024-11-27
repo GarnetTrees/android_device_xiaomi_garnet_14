c541efe74c100fce093c7e5d55b476ed052e5850
royweisfeld <roy.weisfeld+git@hotmail.com>
2024-11-27 22:50:59 +0200

    garnet: Add Viper, BCR, Sim Restart, Logger, and WakacaW (KSU-Next+SUSFS)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,7 +130,7 @@ BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/images/dtbo.img
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 PRODUCT_COPY_FILES += \
-	$(KERNEL_PATH)/images/kernel:kernel
+	$(KERNEL_PATH)/images/WakacaW/v1.5.4/kernel-sunext_susfs:kernel
 
 # Kernel modules
 DLKM_MODULES_PATH := $(KERNEL_PATH)/modules/dlkm
--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -6,34 +6,36 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# RisingOS
-WITH_GMS := true
-TARGET_ENABLE_BLUR := false
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
+
+# Inherit from the MiuiCamera setup
+$(call inherit-product-if-exists, vendor/xiaomi/garnet-miuicamera/products/miuicamera.mk)
 
 PRODUCT_NAME := lineage_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 2312DRA50G
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_SYSTEM_NAME := garnet_global
+PRODUCT_SYSTEM_DEVICE := garnet
 
+# Extra Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_OMX_SERVICE := false
+TARGET_ENABLE_BLUR := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="garnet_global-user 14 UKQ1.231003.002 V816.0.17.0.UNRMIXM release-keys" \
     BuildFingerprint=Redmi/garnet_global/garnet:14/UKQ1.231003.002/V816.0.17.0.UNRMIXM:user/release-keys \
-    DeviceName=garnet \
-    DeviceProduct=garnet_global \
-    RisingChipset="Snapdragon® 7s Gen 2" \
-    RisingMaintainer="Mohammad Kibria"
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
