7ce84aeace366404ffd05e0022c3388fd1b6ff06
royweisfeld <roy.weisfeld+git@hotmail.com>
2024-11-27 22:50:59 +0200

    garnet: Add Viper, BCR and Shorekeeper (KSU)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,7 +135,7 @@ BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/images/dtbo.img
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 PRODUCT_COPY_FILES += \
-	$(KERNEL_PATH)/images/kernel:kernel
+	$(KERNEL_PATH)/images/shorekeeper/v2/kernel-su:kernel
 
 # Kernel modules
 DLKM_MODULES_PATH := $(KERNEL_PATH)/modules/dlkm
