80c2e873f4edda11b767e8cc46b3048d43a01988
Ramadhani <ramadhanifals@gmail.com>
2025-01-07 08:05:41 -0300

    garnet: Build XiaomiDolby
    * Required from hardware/xiaomi
    
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- device.mk
+++ device.mk
@@ -355,6 +355,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
 # Memtrack
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
