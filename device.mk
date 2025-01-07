92b9669a8cf3619b02152708d8827a0f698355ee
kleidione Freitas <kleidione@gmail.com>
2025-01-07 17:01:42 -0300

    garnet: Ship dax-default to device tree
    Signed-off-by: kleidione <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -354,6 +354,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiDolby
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml    
+
 # Memtrack
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
