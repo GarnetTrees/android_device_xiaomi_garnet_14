2b22a9271806a17d8708e3674bd0e635cb517796
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-04 19:07:25 +0300

    Drop BCR

--- device.mk
+++ device.mk
@@ -621,8 +621,5 @@ PRODUCT_BOOT_JARS += \
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
 
-# Inherit BCR
-$(call inherit-product, vendor/bcr/bcr.mk)
-
 # Viper
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
