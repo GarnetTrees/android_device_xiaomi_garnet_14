4416594c0fd9ab4854688b430aabcac6d14a73e7
kleidione Freitas <kleidione@gmail.com>
2025-03-26 09:48:08 -0300

    garnet: Add support for dolby music
    Ref:
    https://github.com/FlamingoOS-Devices/hardware_dolby
    crdroidandroid/android_device_oneplus_sm8550-common@2f8d783
    
    Signed-off-by: Kleidione <kleidione@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -87,10 +87,9 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace(r'=(\d+)>', r'="\1">'),
     ('vendor/etc/init/hw/init.batterysecret.rc', 'vendor/etc/init/hw/init.mi_thermald.rc', 'vendor/etc/init/hw/init.qti.kernel.rc'): blob_fixup()
         .regex_replace(r'on charger', r'on property:init.svc.vendor.charger=running'),
-    'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
-        .regex_replace(r'.+dolby.+\n', ''),
     'vendor/etc/media_codecs_parrot_v0.xml': blob_fixup()
-        .regex_replace(r'.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video).*\n', '')
+        .regex_replace('media_codecs_vendor_audio', 'media_codecs_dolby_audio'),
     'vendor/etc/perf/commonresourceconfigs.xml': blob_fixup()
         .regex_replace('.+<Minor OpcodeValue="0x16" Node="/sys/devices/system/cpu/bus_dcvs/L3/soc:qcom,memlat:l3:prime/min_freq" />+\n', '')
         .regex_replace('.+<Minor OpcodeValue="0x12" Node="/sys/devices/system/cpu/bus_dcvs/LLCC/190b6400.qcom,bwmon-llcc/max_freq" />+\n', '')
@@ -103,6 +102,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('audio.primary.parrot.so', 'audio.primary.garnet.so'),         
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
+    ('vendor/lib64/libstagefright_soft_ddpdec.so', 'vendor/lib64/libdlbdsservice.so', 'vendor/lib64/libstagefright_soft_ac4dec.so', 'vendor/lib64/libstagefrightdolby.so'): blob_fixup()
+        .patchelf_version('0_17_2')
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),     
 }  # fmt: skip
 
 # Module definition
